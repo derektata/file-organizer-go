12546def4224680430081fa7ab82d55b13e178cb
Derek Tata <derekjtata@gmail.com>
2024-08-02 15:53:24 -0400

    ♻️ Refactor code
    Added generics for cleaner code

--- file_organizer.go
+++ file_organizer.go
@@ -32,28 +32,6 @@ func NewFileOrganizer(path string) (*FileOrganizer, error) {
 	}, nil
 }
 
-// MoveFile moves the file to a categorized directory based on its extension and optionally prepends the date to the file name.
-func (o *FileOrganizer) MoveFile(filePath, fileName string, prependDate bool) error {
-	fileExtension := strings.ToLower(filepath.Ext(fileName))
-
-	for category, extensions := range o.FileExtensions {
-		for _, extension := range extensions {
-			if fileExtension == extension {
-				categoryPath := filepath.Join(o.Path, category)
-				newFileName := fileName
-
-				if prependDate {
-					currentTime := time.Now().Format("2006-01-02")
-					newFileName = fmt.Sprintf("%s_%s", currentTime, fileName)
-				}
-
-				return MoveAndCreateDir(filePath, categoryPath, newFileName)
-			}
-		}
-	}
-	return nil
-}
-
 // OrganizeFiles organizes files in the specified directory, optionally prepending the date to the filenames.
 func (o *FileOrganizer) OrganizeFiles(prependDate bool) error {
 	files, err := os.ReadDir(o.Path)
@@ -62,11 +40,54 @@ func (o *FileOrganizer) OrganizeFiles(prependDate bool) error {
 	}
 
 	for _, file := range files {
-		if !file.IsDir() {
-			if err := o.MoveFile(filepath.Join(o.Path, file.Name()), file.Name(), prependDate); err != nil {
-				return fmt.Errorf("failed to move file %s: %v", file.Name(), err)
-			}
+		if file.IsDir() {
+			continue
+		}
+
+		err := o.MoveFile(filepath.Join(o.Path, file.Name()), file.Name(), prependDate)
+		if err != nil {
+			return fmt.Errorf("failed to move file %s: %v", file.Name(), err)
 		}
 	}
 	return nil
+}
+
+// MoveFile moves the file to a categorized directory based on its extension and optionally prepends the date to the file name.
+func (o *FileOrganizer) MoveFile(filePath, fileName string, prependDate bool) error {
+	fileExtension := strings.ToLower(filepath.Ext(fileName))
+
+	category, found := o.findCategory(fileExtension)
+	if !found {
+		return nil
+	}
+
+	categoryPath := filepath.Join(o.Path, category)
+	newFileName := fileName
+
+	if prependDate {
+		currentTime := time.Now().Format("2006-01-02")
+		newFileName = fmt.Sprintf("%s_%s", currentTime, fileName)
+	}
+
+	return MoveAndCreateDir(filePath, categoryPath, newFileName)
+}
+
+// findCategory finds the category for a given file extension.
+func (o *FileOrganizer) findCategory(extension string) (string, bool) {
+	for category, extensions := range o.FileExtensions {
+		if contains(extensions, extension) {
+			return category, true
+		}
+	}
+	return "", false
+}
+
+// contains checks if a slice contains a specific element.
+func contains[T comparable](slice []T, element T) bool {
+	for _, e := range slice {
+		if e == element {
+			return true
+		}
+	}
+	return false
 }
