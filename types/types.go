8d043f1e2677158964d62013aaa0ee0ec3ed1096
Derek Tata <derekjtata@gmail.com>
2024-08-12 18:10:42 -0400

    Define `FileExtensions` type and default config structure
    - Added `FileExtensions` type as a map of file categories to extensions.
    - Included example of default configuration format in comments.

---
+++ types/types.go
@@ -0,0 +1,17 @@
+package types
+
+type FileExtensions map[string][]string
+
+// Default config file:
+// {
+//     "3d-model": 		[],
+//     "application": 	[],
+//     "archive": 		[],
+//     "audio": 		[],
+//     "document": 		[],
+//     "image": 		[],
+//     "presentation": 	[],
+//     "programming": 	[],
+//     "spreadsheet": 	[],
+//     "video": 		[]
+// }
