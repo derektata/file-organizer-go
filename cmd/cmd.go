c60f655c1f5c7cd5cd2c94f3619955ff73a3d5c8
Derek Tata <derekjtata@gmail.com>
2024-08-12 18:09:51 -0400

    Implement command-line interface for file organizer
    - Added `Run` function to handle command-line arguments using `pflag`.
    - Implemented flags for configuration path, directory, and date prepending.
    - Set up `FileOrganizer` with user-provided options and configuration.
    - Included error handling for missing directory and configuration loading.

---
+++ cmd/cmd.go
@@ -0,0 +1,43 @@
+package cmd
+
+import (
+	"file-organizer/config"
+	"file-organizer/pkg"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	flag "github.com/spf13/pflag"
+)
+
+func Run() {
+	var path string
+	var configPath string
+	var prependDate bool
+	// var verbose bool // TODO: Implement verbose mode
+	// var dryRun bool // TODO: Implement dry-run mode
+
+	flag.StringVarP(&configPath, "config", "c", filepath.Join(os.Getenv("HOME"), ".config/file-organizer/config.json"), "The path to the configuration file")
+	flag.StringVarP(&path, "directory", "d", "", "Path to organize files")
+	flag.BoolVarP(&prependDate, "prepend-date", "", false, "Prepend the current date to the file name")
+	// flag.BoolVarP(&verbose, "verbose", "v", false, "Show detailed output in dry-run mode")
+	// flag.BoolVarP(&dryRun, "dry-run", "", false, "Perform a dry run without moving files")
+	flag.Parse()
+
+	if path == "" {
+		fmt.Println("Error: You must specify a directory to organize using the -d flag.")
+		return
+	}
+
+	configLoader, err := config.NewConfigLoader(configPath)
+	pkg.CheckErr(err, "Failed to load configuration: %v", err)
+
+	fileOrganizer := &pkg.FileOrganizer{
+		Path:    path,
+		Config:  configLoader,
+		Options: pkg.OrganizerOptions{PrependDate: prependDate},
+	}
+
+	err = fileOrganizer.OrganizeFiles(prependDate)
+	pkg.CheckErr(err, "Failed to organize files: %v", err)
+}
--- main.go
+++ main.go
@@ -1,27 +1,7 @@
 package main
 
-import (
-	"os"
-	"path/filepath"
-
-	flag "github.com/spf13/pflag"
-)
-
-var configPath string = filepath.Join(os.Getenv("HOME"), ".config/file-organizer/config.json")
+import "file-organizer/cmd"
 
 func main() {
-	var path string
-	var prependDate bool
-	flag.StringVarP(&path, "path", "p", "", "Path to organize files")
-	flag.BoolVarP(&prependDate, "prepend-date", "d", false, "Prepend the current date to the file name")
-	flag.Parse()
-
-	organizer, err := NewFileOrganizer(path)
-	if err != nil {
-		CheckErr(err, "Failed to initialize file organizer")
-	}
-
-	if err := organizer.OrganizeFiles(prependDate); err != nil {
-		CheckErr(err, "Failed to organize files")
-	}
+	cmd.Run()
 }
