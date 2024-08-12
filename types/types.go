22dcdb4b37f4b65b91b9346fd61d76cbe08a594c
Derek Tata <derekjtata@gmail.com>
2024-08-12 18:31:37 -0400

    Remove comment

--- types/types.go
+++ types/types.go
@@ -1,17 +1,3 @@
 package types
 
 type FileExtensions map[string][]string
-
-// Default config file:
-// {
-//     "3d-model": 		[],
-//     "application": 	[],
-//     "archive": 		[],
-//     "audio": 		[],
-//     "document": 		[],
-//     "image": 		[],
-//     "presentation": 	[],
-//     "programming": 	[],
-//     "spreadsheet": 	[],
-//     "video": 		[]
-// }
