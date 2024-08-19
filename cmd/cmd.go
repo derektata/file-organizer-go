9eb278fb4b63e9b0198f63f7e58f3104c27a5059
Derek Tata <derekjtata@gmail.com>
2024-08-19 16:11:57 -0400

    Enable dry-run mode in file organizer
    - Re-enabled the dry-run flag to allow users to perform a dry run without moving files.
    - Removed placeholder comments and unused verbose flag code.
    - Updated the `OrganizeFiles` function call to support dry-run mode.
    
    Details:
    - Added `dryRun` flag to command-line arguments for dry-run functionality.
    - Adjusted `OrganizeFiles` method to accept `dryRun` parameter.

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -14,14 +14,12 @@ func Run() {
 	var path string
 	var configPath string
 	var prependDate bool
-	// var verbose bool // TODO: Implement verbose mode
-	// var dryRun bool // TODO: Implement dry-run mode
+	var dryRun bool
 
 	flag.StringVarP(&configPath, "config", "c", filepath.Join(os.Getenv("HOME"), ".config/file-organizer/config.json"), "The path to the configuration file")
 	flag.StringVarP(&path, "directory", "d", "", "Path to organize files")
 	flag.BoolVarP(&prependDate, "prepend-date", "", false, "Prepend the current date to the file name")
-	// flag.BoolVarP(&verbose, "verbose", "v", false, "Show detailed output in dry-run mode")
-	// flag.BoolVarP(&dryRun, "dry-run", "", false, "Perform a dry run without moving files")
+	flag.BoolVarP(&dryRun, "dry-run", "", false, "Perform a dry run without moving files")
 	flag.Parse()
 
 	if path == "" {
@@ -38,6 +36,6 @@ func Run() {
 		Options: pkg.OrganizerOptions{PrependDate: prependDate},
 	}
 
-	err = fileOrganizer.OrganizeFiles(prependDate)
+	err = fileOrganizer.OrganizeFiles(prependDate, dryRun)
 	pkg.CheckErr(err, "Failed to organize files: %v", err)
 }
