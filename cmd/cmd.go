4a0b22d95f18aa7c8560d59c99b8ff4893fdb06d
Derek Tata <derekjtata@gmail.com>
2025-05-21 08:39:51 -0400

    refactor: simplify flag declarations and streamline variable naming
    Consolidate flag variable declarations into a block for improved readability and consistency. Replace deprecated config path logic with call to config.GetConfigPath(). Also renamed variables for clarity and simplified error messages for better user feedback.

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -4,23 +4,23 @@ import (
 	"file-organizer/config"
 	"file-organizer/pkg"
 	"fmt"
-	"os"
-	"path/filepath"
 
 	flag "github.com/spf13/pflag"
 )
 
 func Run() {
-	var path string
-	var configPath string
-	var prependDate bool
-	var dryRun bool
-	var verbose bool
-	var version bool
+	var (
+		path       string
+		configPath string
+		prepend    bool
+		dryRun     bool
+		verbose    bool
+		version    bool
+	)
 
-	flag.StringVarP(&configPath, "config", "c", filepath.Join(os.Getenv("HOME"), ".config/file-organizer/config.json"), "The path to the configuration file")
+	flag.StringVarP(&configPath, "config", "c", config.GetConfigPath(), "Path to the YAML configuration file")
 	flag.StringVarP(&path, "directory", "d", "", "Path to organize files")
-	flag.BoolVarP(&prependDate, "prepend-date", "", false, "Prepend the current date to the file name")
+	flag.BoolVarP(&prepend, "prepend-date", "", false, "Prepend the current date to the file name")
 	flag.BoolVarP(&dryRun, "dry-run", "", false, "Perform a dry run without moving files")
 	flag.BoolVarP(&verbose, "verbose", "v", false, "Show detailed output")
 	flag.BoolVar(&version, "version", false, "Show the version number")
@@ -31,21 +31,19 @@ func Run() {
 		fmt.Println(Version())
 		return
 	}
-
 	if path == "" {
-		fmt.Println("Error: You must specify a directory to organize using the -d flag.")
+		fmt.Println("Error: you must specify a directory with -d")
 		return
 	}
 
-	configLoader, err := config.NewConfigLoader(configPath)
+	cfg, err := config.NewConfigLoader(configPath)
 	pkg.CheckErr(err, "Failed to load configuration: %v", err)
 
-	fileOrganizer := &pkg.FileOrganizer{
+	org := &pkg.FileOrganizer{
 		Path:    path,
-		Config:  configLoader,
-		Options: pkg.OrganizerOptions{PrependDate: prependDate, DryRun: dryRun, Verbose: verbose},
+		Config:  cfg,
+		Options: pkg.OrganizerOptions{PrependDate: prepend, DryRun: dryRun, Verbose: verbose},
 	}
-
-	err = fileOrganizer.OrganizeFiles()
+	err = org.OrganizeFiles()
 	pkg.CheckErr(err, "Failed to organize files: %v", err)
 }
